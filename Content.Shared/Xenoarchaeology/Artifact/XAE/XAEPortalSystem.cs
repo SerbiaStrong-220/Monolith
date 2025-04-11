9688e8288b1d71f3661e4527519d1520b8df1bda
pa.pecherskij <pa.pecherskij@interfax.ru>
2025-04-12 01:53:25 +0300

    refactor: xml-docs, minor refactors

--- Content.Shared/Xenoarchaeology/Artifact/XAE/XAEPortalSystem.cs
+++ Content.Shared/Xenoarchaeology/Artifact/XAE/XAEPortalSystem.cs
@@ -8,6 +8,9 @@ using Robust.Shared.Timing;
 
 namespace Content.Shared.Xenoarchaeology.Artifact.XAE;
 
+/// <summary>
+/// System for xeno artifact effect that creates temporary portal between places on station.
+/// </summary>
 public sealed class XAEPortalSystem : BaseXAESystem<XAEPortalComponent>
 {
     [Dependency] private readonly IRobustRandom _random = default!;
