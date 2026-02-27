ef32f7e8182f09d513881b53823b7e10c6f64f2e
Lokilife <60302610+Lokilife@users.noreply.github.com>
2026-02-28 02:01:17 +0300

    aaa

--- Content.Client/Guidebook/DocumentParsingManager.cs
+++ Content.Client/Guidebook/DocumentParsingManager.cs
@@ -39,7 +39,6 @@ public sealed partial class DocumentParsingManager
         var whitespaceAndCommentParser = SkipWhitespaces.Then(Try(String("<!--").Then(Parser<char>.Any.SkipUntil(Try(String("-->"))))).SkipMany());
 
         _controlParser = OneOf(_tagParser, TryHeaderControl, TryListControl, TextControlParser)
-             .Before(whitespaceAndCommentParser);
             .Before(whitespaceAndCommentParser);
 
         foreach (var typ in _reflectionManager.GetAllChildren<IDocumentTag>())
