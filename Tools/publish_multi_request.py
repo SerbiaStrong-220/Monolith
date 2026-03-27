a0e1c5d3397ac2521eca278b70bc7a38c1b864ee
Werzet <10843461+Werzet@users.noreply.github.com>
2026-03-27 22:28:32 +1000

    поправил конфигурацию сборки

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,8 +15,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.goobstation.com/"
-FORK_ID = "Monolith"
+ROBUST_CDN_URL = "hhttps://lk.ss220.club/cdn/"
+FORK_ID = "SS220ExodusRelease"
 
 def main():
     parser = argparse.ArgumentParser()
