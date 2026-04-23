8a61b0577871a4e618db1d79b2e283f546254b9a
Renascitur <96677755+dobryninss@users.noreply.github.com>
2026-04-24 01:17:33 +0500

    Update Content.Shared/Weapons/Hitscan/Systems/HitscanBasicRaycastSystem.cs
    Co-authored-by: Maxim Bychkov <60302610+Lokilife@users.noreply.github.com>

--- Content.Shared/Weapons/Hitscan/Systems/HitscanBasicRaycastSystem.cs
+++ Content.Shared/Weapons/Hitscan/Systems/HitscanBasicRaycastSystem.cs
@@ -32,7 +32,12 @@ public sealed class HitscanBasicRaycastSystem : EntitySystem
         var rayCastResults = _physics.IntersectRay(mapCords.MapId, ray, ent.Comp.MaxDistance, shooter, false);
 
         var target = args.Target;
-        var result = rayCastResults.FirstOrNull(hit => hit.HitEntity != gun &&
+        var shooterOrGun = gunComp.UseUserPosition ? shooter : args.Gun;
+        var rayCastResults = _physics.IntersectRay(mapCords.MapId, ray, ent.Comp.MaxDistance, shooterOrGun, false);
+        var target = args.Target;
+        var result = _container.IsEntityOrParentInContainer(shooterOrGun)
+            ? rayCastResults.FirstOrNull()
+            : rayCastResults.FirstOrNull(hit => hit.HitEntity == target || CompOrNull<RequireProjectileTargetComponent>(hit.HitEntity)?.Active != true);
                                                        (hit.HitEntity == target
                                                        || CompOrNull<RequireProjectileTargetComponent>(hit.HitEntity)?.Active != true));
 
