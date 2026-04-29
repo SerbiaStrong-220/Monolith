fa7a7ffbcdda6f2fa2643e2eb56b47899f68666e
JunJun <nekitnea@gmail.com>
2026-04-29 17:23:58 +0300

    fixes

--- Content.Client/_Exodus/Guidebook/Richtext/LocIdTag.cs
+++ Content.Client/_Exodus/Guidebook/Richtext/LocIdTag.cs
@@ -1,42 +1,19 @@
-using System.Diagnostics.CodeAnalysis;
-using Content.Client.UserInterface.RichText;
-using JetBrains.Annotations;
-using Robust.Client.UserInterface;
-using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.RichText;
 using Robust.Shared.Utility;
 
 namespace Content.Client._Exodus.Guidebook.RichText;
 
-[UsedImplicitly]
 public sealed class LocIdTag : IMarkupTagHandler
 {
     public string Name => "locid";
 
-    private static readonly Type[] AllowedTags = new Type[] {
-            typeof(BoldItalicTag),
-            typeof(BoldTag),
-            typeof(BulletTag),
-            typeof(ColorTag),
-            typeof(HeadingTag),
-            typeof(ItalicTag),
-            typeof(MonoTag),
-            typeof(LocIdTag),
-        };
-
     /// <inheritdoc/>
-    public bool TryCreateControl(MarkupNode node, [NotNullWhen(true)] out Control? control)
+    public string TextBefore(MarkupNode node)
     {
+        // Do nothing with an empty tag
         if (!node.Value.TryGetString(out var text))
-        {
-            control = null;
-            return false;
-        }
+            return string.Empty;
 
-        var label = new RichTextLabel();
-        label.SetMessage(Loc.GetString(text), tagsAllowed: AllowedTags);
-
-        control = label;
-        return true;
+        return Loc.GetString(text);
     }
 }
