6edeb35da56ec983bdba23bfe2cf56edebe1512e
Renascitur <96677755+dobryninss@users.noreply.github.com>
2026-05-10 14:28:09 +0500

    Comment

--- Content.Server/Silicons/Borgs/BorgSystem.Modules.cs
+++ Content.Server/Silicons/Borgs/BorgSystem.Modules.cs
@@ -225,8 +225,10 @@ public sealed partial class BorgSystem
             _hands.AddHand(chassis, handId, HandLocation.Middle, hands);
             _hands.DoPickup(chassis, hands.Hands[handId], item, hands);
             EnsureComp<UnremoveableComponent>(item);
-            if (HasComp<BorgModuleStackRechargerComponent>(uid)) // Exodus borg-stack-split-fix
+            // Exodus-begin borg-stack-split-fix
+            if (HasComp<BorgModuleStackRechargerComponent>(uid))
                 EnsureComp<UnsplittableComponent>(item);
+            // Exodus-end
             component.ProvidedItems.Add(handId, item);
         }
 
