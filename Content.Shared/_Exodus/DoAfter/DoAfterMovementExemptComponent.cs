b385e752fd66f9150502df59ea89621651fdf0da
Renascitur <96677755+dobryninss@users.noreply.github.com>
2026-07-27 20:12:17 +0500

    IPC buff

--- Content.Shared/_Exodus/DoAfter/DoAfterMovementExemptComponent.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._Exodus.DoAfter;
-
-/// <summary>
-/// Allows an entity to continue do-afters that would otherwise be cancelled by its movement.
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class DoAfterMovementExemptComponent : Component;
