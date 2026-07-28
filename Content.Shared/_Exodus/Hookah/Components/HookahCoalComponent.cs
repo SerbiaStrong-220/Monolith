2843499e6b106969f43ea273617cb6c3b5322453
Renascitur <96677755+dobryninss@users.noreply.github.com>
2026-07-28 13:35:00 +0500

    hookah

---
+++ Content.Shared/_Exodus/Hookah/Components/HookahCoalComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace Content.Shared._Exodus.Hookah.Components;
+
+[RegisterComponent]
+public sealed partial class HookahCoalComponent : Component
+{
+    [DataField]
+    public float FuelLeft = 120f;
+
+    [DataField]
+    public float FuelDrainIdle = 0.5f;
+}
+
