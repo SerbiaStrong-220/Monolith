186cd430c407210990f7d76bddd7195eccb09d19
Renascitur <96677755+dobryninss@users.noreply.github.com>
2026-07-30 01:26:20 +0500

    Standing

--- Content.Shared/_Exodus/DoAfter/DoAfterInterruptionExemptSystem.cs
+++ Content.Shared/_Exodus/DoAfter/DoAfterInterruptionExemptSystem.cs
@@ -1,10 +1,13 @@
 using Content.Shared.DoAfter;
 using Content.Shared.Movement.Systems;
+using Content.Shared._White.Standing;
+using Robust.Shared.Player;
 
 namespace Content.Shared._Exodus.DoAfter;
 
 public sealed class DoAfterInterruptionExemptSystem : EntitySystem
 {
+    [Dependency] private SharedDoAfterSystem _doAfter = default!;
     [Dependency] private MovementSpeedModifierSystem _movementSpeed = default!;
 
     public override void Initialize()
@@ -13,6 +16,7 @@ public sealed class DoAfterInterruptionExemptSystem : EntitySystem
         SubscribeLocalEvent<DoAfterInterruptionExemptComponent, GetDoAfterInterruptionBreakEvent>(OnGetDoAfterInterruptionBreak);
         SubscribeLocalEvent<DoAfterInterruptionExemptComponent, DoAfterMovementSlowdownChangedEvent>(OnDoAfterMovementSlowdownChanged);
         SubscribeLocalEvent<DoAfterInterruptionExemptComponent, RefreshMovementSpeedModifiersEvent>(OnRefreshMovementSpeedModifiers);
+        SubscribeNetworkEvent<ChangeLayingDownEvent>(OnChangeLayingDown);
     }
 
     private void OnGetDoAfterInterruptionBreak(Entity<DoAfterInterruptionExemptComponent> ent,
@@ -29,6 +33,35 @@ public sealed class DoAfterInterruptionExemptSystem : EntitySystem
 
         if (ent.Comp.Exemptions.HasFlag(DoAfterInterruptionExemptions.DropItem))
             args.BreakOnDropItem = false;
+    }
+
+    private void OnChangeLayingDown(ChangeLayingDownEvent ev, EntitySessionEventArgs args)
+    {
+        if (args.SenderSession.AttachedEntity is not { } uid ||
+            !TryComp<DoAfterInterruptionExemptComponent>(uid, out var exemption))
+        {
+            return;
+        }
+
+        CancelMobileDoAfters((uid, exemption));
+    }
+
+    private void CancelMobileDoAfters(Entity<DoAfterInterruptionExemptComponent> ent)
+    {
+        if (!TryComp<DoAfterComponent>(ent, out var doAfter))
+            return;
+
+        var toCancel = new List<ushort>();
+        foreach (var (id, active) in doAfter.DoAfters)
+        {
+            if (active.Args.ApplyMovementSlowdown && !active.Cancelled && !active.Completed)
+                toCancel.Add(id);
+        }
+
+        foreach (var id in toCancel)
+        {
+            _doAfter.Cancel(ent.Owner, id, doAfter);
+        }
     }
 
     private void OnDoAfterMovementSlowdownChanged(Entity<DoAfterInterruptionExemptComponent> ent,
