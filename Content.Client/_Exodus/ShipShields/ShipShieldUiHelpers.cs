e6006a6589db5d33e6a6384cde7258a8a754f018
Renascitur <96677755+dobryninss@users.noreply.github.com>
2026-08-03 12:12:01 +0500

    interpolate

--- Content.Client/_Exodus/ShipShields/ShipShieldUiHelpers.cs
+++ Content.Client/_Exodus/ShipShields/ShipShieldUiHelpers.cs
@@ -9,11 +9,10 @@ public static class ShipShieldUiHelpers
 
     public static Color GetHealthColor(float healthFraction)
     {
-        if (healthFraction <= 0.25f)
-            return Color.Red;
+        var fraction = Math.Clamp(healthFraction, 0f, 1f);
 
-        return healthFraction <= 0.5f
-            ? Color.Orange
-            : Color.Green;
+        return fraction <= 0.5f
+            ? Color.InterpolateBetween(Color.Red, Color.Yellow, fraction * 2f)
+            : Color.InterpolateBetween(Color.Yellow, Color.Lime, (fraction - 0.5f) * 2f);
     }
 }
