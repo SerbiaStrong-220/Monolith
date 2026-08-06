6e9f9dca390dd062cd74677202901af17ba16794
Ceaser <lajk0@mail.ru>
2026-08-07 00:06:31 +0300

    - add holo projector Generic

--- Content.Server/Holosign/HolosignSystem.cs
+++ Content.Server/Holosign/HolosignSystem.cs
@@ -52,7 +52,8 @@ public sealed partial class HolosignSystem : EntitySystem
         // overlapping of the same holo on one tile remains allowed to allow holofan refreshes
         var holoUid = EntityManager.SpawnEntity(component.SignProto, args.ClickLocation.SnapToGrid(EntityManager));
         var xform = Transform(holoUid);
-        if (!xform.Anchored)
+        // if (!xform.Anchored)
+        if (component.IsAnchoredOnSpawn && !xform.Anchored) // Exodus
             _transform.AnchorEntity(holoUid, xform); // anchor to prevent any tempering with (don't know what could even interact with it)
 
         args.Handled = true;
